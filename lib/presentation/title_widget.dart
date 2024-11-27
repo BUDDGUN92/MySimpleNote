821109c62de2d651c59d5ed7a04de130a9520667
BUDDGUN92 <gunawardena.buddika@yandex.ru>
2024-11-27 23:30:23 +0530

    Decresed App Title font size

--- lib/presentation/title_widget.dart
+++ lib/presentation/title_widget.dart
@@ -1,17 +1,3 @@
-/// A widget that displays the application title "My Simple Note".
-///
-/// This widget creates a centered text display with specific styling:
-/// - Black color (ARGB: 255, 0, 0, 0)
-/// - Font size of 50
-/// - Bold font weight
-/// - Helvetica Neue font family
-///
-/// Used as the header or title component in the application.
-///
-/// Example:
-/// ```dart
-/// TitleWidget()
-/// ```
 import 'package:flutter/material.dart';
 
 class TitleWidget extends StatelessWidget {
@@ -24,7 +10,7 @@ class TitleWidget extends StatelessWidget {
       textAlign: TextAlign.center,
       style: TextStyle(
         color: Color.fromARGB(255, 0, 0, 0),
-        fontSize: 50,
+        fontSize: 32,
         fontWeight: FontWeight.bold,
         fontFamily: 'Helvetica Neue',
       ),
